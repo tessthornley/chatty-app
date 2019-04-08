ec7476a0327cf64563496a54338176653d2e58f6
Tess <tess.thornley@gmail.com>
2019-04-08 23:57:40 +0000

    HTML code sectioned off and messages mapped

---
+++ src/ChatBar.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ChatBar extends Component {
+  // constructor(props) {
+  //   super();
+  // }
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+export default ChatBar;
---
+++ src/Message.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  constructor(props) {
+  super(props)
+  }
+  render() {
+    return (
+      <div className="message">
+        <span className="message-username">{this.props.username}</span>
+        <span className="message-content">{this.props.content}</span>
+      </div>
+    );
+  }
+}
+export default Message;
